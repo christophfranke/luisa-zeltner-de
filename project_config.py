e0bac8f354da45de7b084c166e9384297f70244a
Christoph Franke <christophfranke123@googlemail.com>
2016-12-12 19:09:25 +0100

    Update scripts and config

--- project_config.py
+++ project_config.py
@@ -3,4 +3,15 @@
 LOCAL_DB_NAME = 'luisazeltner.de'
 
 LOCAL_DOMAIN = 'luisazeltner.local'
-REMOTE_DOMAIN = 'luisazeltner.de'
+LIVE_DOMAIN = 'luisazeltner.de'
+
+FTP_USER = 'ftp12538803-luisa'
+FTP_PASSWORD = 'pause-im-glas'
+FTP_PATH_TO_WWW_DIR = '.'
+FTP_HOST = 'luisazeltner.de'
+
+REMOTE_DB_NAME = 'db12538803-luisa'
+REMOTE_DB_USER = 'db12538803-luisa'
+REMOTE_DB_PASSWORD = 'sahnetorte'
+
+SECURITY_HASH = 'XqESWl1LBY1XBduww6afOu7kiSNsAmF9' #added automatically on 2016-08-03 21:11:46.671511
